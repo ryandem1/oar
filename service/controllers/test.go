8242341c2981f3a2087696c8501d99ceb836e588
ryandemarigny <ryandemarigny@gmail.com>
2023-01-07 23:30:07 -0800

    Updated GetTests to use database

--- service/controllers/test.go
+++ service/controllers/test.go
@@ -134,5 +134,27 @@ func (tc *TestController) DeleteTests(c *gin.Context) {
 }
 
 func (tc *TestController) GetTests(c *gin.Context) {
+	conn, err := tc.DBPool.Acquire()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	defer tc.DBPool.Release(conn)
+
+	var tests []*models.Test
+	rows, err := conn.Query("SELECT * FROM tests")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	for rows.Next() {
+		test := &models.Test{}
+		err = rows.Scan(&test.ID, &test.Summary, &test.Outcome, &test.Analysis, &test.Resolution, &test.Doc)
+		if err != nil {
+			return
+		}
+		tests = append(tests, test)
+	}
+
 	c.JSON(http.StatusOK, tests)
 }
