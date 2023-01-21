3a0710e28f307101cc010520562946011af4c263
ryandemarigny <ryandemarigny@gmail.com>
2023-01-21 08:21:21 -0800

    Installed flowbite-svelte component library

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+    import { Alert } from 'flowbite-svelte';
+</script>
 
+<div class="p-8">
+    <Alert>
+        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </Alert>
+</div>
