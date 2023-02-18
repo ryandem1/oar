a2ad42e05cdae75918b8bc1bcc98b58d8b57002c
ryandemarigny <ryandemarigny@gmail.com>
2023-02-18 15:00:35 -0800

    Implemented unit test TestInvalidTestsDoNotPassValidate

--- service/models_test.go
+++ service/models_test.go
@@ -20,11 +20,35 @@ func TestValidTestsPassValidate(t *testing.T) {
 
 // TestInvalidTestsDoNotPassValidate will ensure that the Test.Validate() does not accept invalid Test objects.
 func TestInvalidTestsDoNotPassValidate(t *testing.T) {
-	tests := multiple(15, Fake.test) // Start will valid tests and make them invalid
+	validTests := multiple(15, Fake.test) // Start will valid tests and make them invalid
 
-	invalidTest1 := tests[0]
-	invalidTest1.Outcome = "Skipped" // OAR doesn't accept skipped tests
+	// Alter tests to make them invalid (each test can be thought of as its own scenario)
+	validTests[0].Outcome = "Skipped" // OAR doesn't accept skipped tests
+	validTests[1].Analysis = "InvalidAnalysis"
+	validTests[2].Resolution = "InvalidResolution"
 
-	invalidTest2 := tests[1]
-	invalidTest2.ID = -5 // IDs should always be positive
+	if validTests[3].Outcome == "Passed" {
+		validTests[3].Analysis = TruePositive // Passed tests cannot have a "positive" analysis
+	} else {
+		validTests[3].Analysis = TrueNegative // Failed tests cannot have a "Negative" analysis
+	}
+
+	validTests[4].Summary = ""
+
+	invalidTests := map[string]*Test{
+		"Invalid Outcome Test":      validTests[0],
+		"Invalid Analysis Test":     validTests[1],
+		"Invalid Resolution Test":   validTests[2],
+		"Outcome/Analysis Mismatch": validTests[3],
+		"Empty Summary Test":        validTests[4],
+	}
+
+	for testName, invalidTest := range invalidTests {
+		t.Run(testName, func(t *testing.T) {
+			err := invalidTest.Validate()
+			if err == nil {
+				t.Errorf("Scenario %s failed, invalid test did not throw error", testName)
+			}
+		})
+	}
 }
