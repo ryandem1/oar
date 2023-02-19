2768870b0c555db54dcd5f2f3bc8d721732a705b
ryandemarigny <ryandemarigny@gmail.com>
2023-02-18 16:49:33 -0800

    Implemented unit test TestDeleteTests

--- service/postgres_test.go
+++ service/postgres_test.go
@@ -3,9 +3,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jackc/pgx"
 	"testing"
 )
+
+// getPoolFromConfig will return a new pgx.ConnPool after getting a NewConfig from the environment
+func getPoolFromConfig() (*pgx.ConnPool, error) {
+	config, err := NewConfig() // Gets config from environment
+	if err != nil {
+		return nil, fmt.Errorf("error obtaining config. error: %s", err)
+	}
+
+	pgPool, err := NewPGPool(config.PG)
+	if err != nil {
+		return nil, fmt.Errorf("error obtaining pg connection. error: %s", err)
+	}
+	return pgPool, err
+}
 
 // TestNewPGPoolPositive ensures NewPGPool works with a valid config
 func TestNewPGPoolPositive(t *testing.T) {
@@ -65,16 +80,11 @@ func TestNewPGPoolNegative(t *testing.T) {
 	}
 }
 
-// TestSelectTests will ensure that we can select valid tests that are in postgres
-func TestSelectTests(t *testing.T) {
+// TestSelectCreateTests will ensure that we can select valid tests that are in postgres
+func TestSelectCreateTests(t *testing.T) {
 	amountOfTests := 5 // number of tests to create/read
 
-	config, err := NewConfig() // Gets config from environment
-	if err != nil {
-		t.Error("error obtaining config", err)
-	}
-
-	pgPool, err := NewPGPool(config.PG)
+	pgPool, err := getPoolFromConfig()
 	if err != nil {
 		t.Error("error obtaining pg connection", err)
 	}
@@ -102,5 +112,55 @@ func TestSelectTests(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
+	}
+}
+
+// TestDeleteTests will ensure that we can delete tests with DeleteTests. Also ensures that if you attempt to delete
+// tests that are already deleted, it will just return 0 rows affected with no errors.
+func TestDeleteTests(t *testing.T) {
+	amountOfTests := 5 // number of tests to create/read
+
+	pgPool, err := getPoolFromConfig()
+	if err != nil {
+		t.Error("error obtaining pg connection", err)
+	}
+
+	validTests := multiple(amountOfTests, Fake.test)
+
+	for _, validTest := range validTests {
+		err = InsertTest(pgPool, validTest)
+		if err != nil {
+			t.Error("error during data setup", err)
+		}
+	}
+
+	testsToDelete, err := SelectTests(
+		pgPool,
+		"select * from oar_tests order by created desc limit $1",
+		amountOfTests,
+	)
+
+	var testIDsToDelete []uint64
+
+	for _, testToDelete := range testsToDelete {
+		testIDsToDelete = append(testIDsToDelete, testToDelete.ID)
+	}
+
+	rowsDeleted, err := DeleteTests(pgPool, testIDsToDelete)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if rowsDeleted != int64(amountOfTests) {
+		t.Error("not all tests were deleted")
+	}
+
+	rowsDeleted, err = DeleteTests(pgPool, testIDsToDelete)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if rowsDeleted != 0 {
+		t.Error("consecutive delete call deleted more rows")
 	}
 }
