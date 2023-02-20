0f2cd58cf308b2cd0c526732bc173028083c767e
ryandemarigny <ryandemarigny@gmail.com>
2023-02-19 17:54:58 -0800

    Added basic models

---
+++ pytest/pytest_oar/models.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Any
+
+
+class Outcome(str, Enum):
+    Passed = "Passed"
+    Failed = "Failed"
+
+
+class Analysis(str, Enum):
+    NotAnalyzed = "NotAnalyzed"
+    TruePositive = "TruePositive"
+    FalsePositive = "FalsePositive"
+    TrueNegative = "TrueNegative"
+    FalseNegative = "FalseNegative"
+
+
+class Resolution(str, Enum):
+    Unresolved = "Unresolved"
+    NotNeeded = "NotNeeded"
+    TicketCreated = "TicketCreated"
+    QuickFix = "QuickFix"
+    KnownIssue = "KnownIssue"
+    TestFixed = "TestFixed"
+    TestDisabled = "TestDisabled"
+
+
+class OARTest(BaseModel):
+    id_: int
+    summary: str
+    outcome: Outcome
+    analysis: Analysis
+    resolution: Resolution
+    doc: dict[str, Any]
+
