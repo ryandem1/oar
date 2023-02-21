9218f3d0fc3bd2fbc65de064bd6db09fd0d2382d
ryandemarigny <ryandemarigny@gmail.com>
2023-02-20 17:36:30 -0800

    Implement multi-test type support

--- pytest/oar/plugin.py
+++ pytest/oar/plugin.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 
 from pytest import fixture, FixtureRequest, hookimpl, Item, CallInfo, StashKey, CollectReport
 from oar.client import Client
@@ -45,20 +46,37 @@ def oar_test(request: FixtureRequest, oar_client) -> Test:
 
     More info on properties:
 
-    1. Tests begin NotAnalyzed and Unresolved
+    1. Tests default to NotAnalyzed and Unresolved
     2. If a test fails in the setup phase, the test's analysis will be marked as a FalsePositive by default
     3. If a test fails in the call phase, the test will remain NotAnalyzed
     4. If a test fails in the teardown phase, the test's analysis will be marked as a FalsePositive by default
     5. If a test passes, it is by default a TrueNegative
     6. If outcome or analysis were set during the test runtime, they will not be set by the above logic
+    7. If a test is set as a specific type, the "type" will automatically be added if not already defined
 
     Yields
     -------
     test : Test
         Current OAR test to add attributes onto
     """
-    test = Test(analysis=Analysis.NotAnalyzed, resolution=Resolution.Unresolved)
+    # This will get the type hint of the `oar_test` fixture, defaulting to `oar.Test` if one was not provided
+    test_type = typing.get_type_hints(request.function).get(oar_test.__name__, Test)
+
+    # Ensures that a test type is actually being used
+    if test_type != Test and Test not in test_type.__bases__:
+        raise TypeError("`oar_test` fixture type must inherit from `oar.Test`")
+
+    # Initialize a base test container to yield back into the test to use, the specific type will be used later
+    test = Test()
     yield test
+
+    # Initializes the specific test_type
+    test = test_type(**test.dict())
+
+    # Appends a test type onto a test if it is a specific type
+    if type(test) != Test and "type" not in test.__dict__:
+        test.type = type(test).__name__
+
     # request.node is an "item" because we use the default "function" scope
     report = request.node.stash[phase_report_key]
 
@@ -68,21 +86,31 @@ def oar_test(request: FixtureRequest, oar_client) -> Test:
     if report["setup"].failed:
         if test.outcome is None:
             test.outcome = Outcome.Failed
-        if test.analysis == Analysis.NotAnalyzed:
+        if test.analysis is None:
             test.analysis = Analysis.FalsePositive
+        if test.resolution is None:
+            test.resolution = Resolution.Unresolved
     elif ("call" not in report) or report["call"].failed:
         if test.outcome is None:
             test.outcome = Outcome.Failed
+        if test.analysis is None:
+            test.analysis = Analysis.NotAnalyzed
+        if test.resolution is None:
+            test.resolution = Resolution.Unresolved
     elif "teardown" in report and report["teardown"].failed:
         if test.outcome is None:
             test.outcome = Outcome.Failed
-        if test.analysis == Analysis.NotAnalyzed:
+        if test.analysis is None:
             test.analysis = Analysis.FalsePositive
+        if test.resolution is None:
+            test.resolution = Resolution.Unresolved
     else:
         if test.outcome is None:
             test.outcome = Outcome.Passed
-        if test.analysis == Analysis.NotAnalyzed:
+        if test.analysis is None:
             test.analysis = Analysis.TrueNegative
+        if test.resolution is None:
+            test.resolution = Resolution.NotNeeded
 
     test_id = oar_client.add_test(test)
     logger.info(f"OAR Test Result ID: {test_id}")
