6270258c099603147ce98b711b5a3ef18b296e59
ryandemarigny <ryandemarigny@gmail.com>
2023-05-01 19:05:06 -0700

    Got to 100% coverage and squashed some bugs

--- pytest/tests/test_client.py
+++ pytest/tests/test_client.py
@@ -1,5 +1,6 @@
 import typing
 
+import pytest
 import requests_mock
 
 import oar
@@ -54,3 +55,141 @@ class TestClient:
             query_string = client.query(query)
 
         assert oar.TestQuery.from_query_string(query_string) == query
+
+    def test_query_soft_fails(self, valid_query: dict[str, typing.Any]):
+        """
+        Test that the /query handler fails softly
+
+        Parameters
+        ----------
+        valid_query: dict[str, typing.Any]
+            Query
+        """
+        query = oar.TestQuery(**valid_query)
+        client = oar.Client("http://localhost:8080")
+
+        with requests_mock.Mocker() as m:
+            m.post("http://localhost:8080/query", status_code=400)
+            query_string = client.query(query)
+
+        assert not query_string
+
+    def test_get_tests(self, valid_query: dict[str, typing.Any], valid_test: dict[str, typing.Any]):
+        """
+        Test that the GET /tests handler works
+
+        Parameters
+        ----------
+        valid_query: dict[str, typing.Any]
+            Query
+
+        valid_test: dict[str, typing.Any]
+            Test
+        """
+        query = oar.TestQuery(**valid_query)
+        client = oar.Client("http://localhost:8080")
+        query_result = oar.TestQueryResult(
+            count=1,
+            tests=[oar.Test(**valid_test)]
+        )
+
+        with requests_mock.Mocker() as m:
+            m.get(
+                url=f"http://localhost:8080/tests",
+                json=query_result.dict()
+            )
+            results = client.get_tests(query)
+
+        assert results == query_result
+
+    def test_get_tests_soft_fail(self, valid_query: dict[str, typing.Any], valid_test: dict[str, typing.Any]):
+        """
+        Test that the GET /tests handler fails softly
+
+        Parameters
+        ----------
+        valid_query: dict[str, typing.Any]
+            Query
+
+        valid_test: dict[str, typing.Any]
+            Test
+        """
+        query = oar.TestQuery(**valid_query)
+        client = oar.Client("http://localhost:8080")
+
+        with requests_mock.Mocker() as m:
+            m.get(
+                url=f"http://localhost:8080/tests",
+                status_code=400
+            )
+            results = client.get_tests(query)
+
+        assert not results
+
+    @pytest.mark.parametrize("json_data", [True, False])
+    def test_invalid_response_logging(self, valid_query: dict[str, typing.Any], json_data):
+        """
+        Test that logging paths work correctly for invalid responses
+
+        Parameters
+        ----------
+        valid_query: dict[str, typing.Any]
+            Test
+        """
+        query = oar.TestQuery(**valid_query)
+        client = oar.Client("http://localhost:8080")
+
+        with requests_mock.Mocker() as m:
+            m.get(
+                url=f"http://localhost:8080/tests",
+                status_code=400,
+                json={"error": "some fake error"} if json_data else None,
+                text="{invalid}" if not json_data else None
+            )
+            client.get_tests(query)
+
+    def test_enrich_tests(self, valid_query: dict[str, typing.Any], valid_test: dict[str, typing.Any]):
+        """
+        Test that the PATCH /tests handler works
+
+        Parameters
+        ----------
+        valid_query: dict[str, typing.Any]
+            Query
+
+        valid_test: dict[str, typing.Any]
+            Test
+        """
+        query = oar.TestQuery(**valid_query)
+        test = oar.Test(**valid_test)
+        client = oar.Client("http://localhost:8080")
+
+        with requests_mock.Mocker() as m:
+            m.patch(
+                url=f"http://localhost:8080/tests",
+                status_code=200
+            )
+            response_code = client.enrich_tests(test, query)
+
+        assert response_code
+
+    def test_delete_tests(self, valid_query: dict[str, typing.Any]):
+        """
+        Test that the DELETE /tests handler works
+
+        Parameters
+        ----------
+        valid_query: dict[str, typing.Any]
+            Query
+        """
+        query = oar.TestQuery(**valid_query)
+        client = oar.Client("http://localhost:8080")
+
+        with requests_mock.Mocker() as m:
+            m.delete(
+                url=f"http://localhost:8080/tests",
+                status_code=200
+            )
+            response_code = client.delete_tests(query)
+
+        assert response_code
