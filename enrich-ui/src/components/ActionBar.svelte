b01adc488f480688783cecfc48ddbb8927358f5a
ryandemarigny <ryandemarigny@gmail.com>
2023-05-11 21:55:55 -0700

    Made ActionBar its own component

---
+++ enrich-ui/src/components/ActionBar.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { AppBar } from '@skeletonlabs/skeleton';
+  import { version } from '$app/environment';
+</script>
+
+<AppBar background="bg-secondary-500" class="rounded-md" shadow="shadow-md">
+  <svelte:fragment slot="lead">
+    <img src="oarLogo.png" alt="OAR Logo" class="h-10 w-10" />
+    <div class="pl-2 ">OAR Enrich v{version}</div>
+  </svelte:fragment>
+</AppBar>
--- enrich-ui/src/routes/+layout.svelte
+++ enrich-ui/src/routes/+layout.svelte
@@ -1,30 +1,24 @@
 <script>
 
   // Your selected Skeleton theme:
-  import '../theme.postcss';
+  import "../theme.postcss";
 
   // This contains the bulk of Skeletons required styles:
-  import '@skeletonlabs/skeleton/styles/all.css';
+  import "@skeletonlabs/skeleton/styles/all.css";
 
   // Finally, your application's global stylesheet (sometimes labeled 'app.css')
-  import '../app.postcss';
+  import "../app.postcss";
 
-  import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+  import { AppShell } from "@skeletonlabs/skeleton";
 
-  import { version } from '$app/environment';
-
+  import { ActionBar } from "$components/ActionBar.svelte";
 </script>
 
 
 <AppShell regionPage="relative p-8 pr-10 bg-tertiary-50" slotPageHeader="sticky top-0 z-10">
 
   <svelte:fragment slot="pageHeader">
-    <AppBar background="bg-secondary-500" class="rounded-md" shadow="shadow-md">
-      <svelte:fragment slot="lead">
-        <img src="oarLogo.png" alt="OAR Logo" class="h-10 w-10" />
-        <div class="pl-2 ">OAR Enrich v{version}</div>
-      </svelte:fragment>
-    </AppBar>
+    <ActionBar />
   </svelte:fragment>
 
   <slot />
