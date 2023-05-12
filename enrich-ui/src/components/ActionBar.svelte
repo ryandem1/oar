28789ef04d6c0ac2f43954fc30a9cc44609140e5
ryandemarigny <ryandemarigny@gmail.com>
2023-05-11 22:34:30 -0700

    Added ActionBar back with icons

---
+++ enrich-ui/src/components/ActionBar.svelte
@@ -0,0 +1,16 @@
+<script>
+import { version } from "$app/environment";
+import { AppBar } from "@skeletonlabs/skeleton";
+import Icon from "./Icon.svelte";
+</script>
+
+<AppBar background="bg-secondary-500" class="rounded-md" shadow="shadow-md">
+  <svelte:fragment slot="lead">
+    <img src="oarLogo.png" alt="OAR Logo" class="h-10 w-10" />
+    <div class="pl-2 ">OAR Enrich v{version}</div>
+  </svelte:fragment>
+  <svelte:fragment slot="trail">
+    <button type="button" class="btn bg-primary-300"><Icon name="edit"/></button>
+    <button type="button" class="btn bg-error-300"><Icon name="trash"/></button>
+  </svelte:fragment>
+</AppBar>
--- enrich-ui/src/routes/+layout.svelte
+++ enrich-ui/src/routes/+layout.svelte
@@ -9,24 +9,15 @@
   // Finally, your application's global stylesheet (sometimes labeled 'app.css')
   import "../app.postcss";
 
-  import { AppBar, AppShell } from "@skeletonlabs/skeleton";
-  import { version } from "$app/environment";
+  import { AppShell } from "@skeletonlabs/skeleton";
+  import ActionBar from "../components/ActionBar.svelte";
 </script>
 
 
 <AppShell regionPage="relative p-8 pr-10 bg-tertiary-50" slotPageHeader="sticky top-0 z-10">
 
   <svelte:fragment slot="pageHeader">
-    <AppBar background="bg-secondary-500" class="rounded-md" shadow="shadow-md">
-      <svelte:fragment slot="lead">
-        <img src="oarLogo.png" alt="OAR Logo" class="h-10 w-10" />
-        <div class="pl-2 ">OAR Enrich v{version}</div>
-      </svelte:fragment>
-      <svelte:fragment slot="trail">
-        <button type="button" class="btn bg-primary-300">Enrich</button>
-        <button type="button" class="btn bg-error-300">Delete</button>
-      </svelte:fragment>
-    </AppBar>
+    <ActionBar />
   </svelte:fragment>
 
   <slot />
