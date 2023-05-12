a0e3815de31363a72fb174de5bcfcf6bfd8c2586
ryandemarigny <ryandemarigny@gmail.com>
2023-05-11 22:06:48 -0700

    Moved ActionBar back to layout because it didn't work

--- enrich-ui/src/routes/+layout.svelte
+++ enrich-ui/src/routes/+layout.svelte
@@ -9,16 +9,24 @@
   // Finally, your application's global stylesheet (sometimes labeled 'app.css')
   import "../app.postcss";
 
-  import { AppShell } from "@skeletonlabs/skeleton";
-
-  import { ActionBar } from "$components/ActionBar.svelte";
+  import { AppBar, AppShell } from "@skeletonlabs/skeleton";
+  import { version } from "$app/environment";
 </script>
 
 
 <AppShell regionPage="relative p-8 pr-10 bg-tertiary-50" slotPageHeader="sticky top-0 z-10">
 
   <svelte:fragment slot="pageHeader">
-    <ActionBar />
+    <AppBar background="bg-secondary-500" class="rounded-md" shadow="shadow-md">
+      <svelte:fragment slot="lead">
+        <img src="oarLogo.png" alt="OAR Logo" class="h-10 w-10" />
+        <div class="pl-2 ">OAR Enrich v{version}</div>
+      </svelte:fragment>
+      <svelte:fragment slot="trail">
+        <button type="button" class="btn bg-primary-300">Enrich</button>
+        <button type="button" class="btn bg-error-300">Delete</button>
+      </svelte:fragment>
+    </AppBar>
   </svelte:fragment>
 
   <slot />
