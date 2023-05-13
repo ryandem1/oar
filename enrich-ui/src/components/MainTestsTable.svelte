6229a7241136bf27dc57e673fb627ed0d6829cf9
ryandemarigny <ryandemarigny@gmail.com>
2023-05-13 13:00:00 -0700

    Selected animation is good enough

--- enrich-ui/src/components/MainTestsTable.svelte
+++ enrich-ui/src/components/MainTestsTable.svelte
@@ -49,15 +49,19 @@
   SELECT FUNCTIONALITY
   */
   let selectedTestIDs: number[];
+  let selectedTestIdxes: number[];
   $: selectedTestIDs = [];
+  $: selectedTestIdxes = [];
 
-  function toggleRow(row: string[]) {
+  function toggleRow(row: string[], index: number) {
     let testID = to_number(row[fields.indexOf("id")])
 
     if (selectedTestIDs.includes(testID)) {
       selectedTestIDs = selectedTestIDs.filter(i => i !== testID);
+      selectedTestIdxes = selectedTestIdxes.filter(i => i !== index)
     } else {
       selectedTestIDs = [...selectedTestIDs, testID];
+      selectedTestIdxes = [...selectedTestIdxes, index];
     }
   }
 </script>
@@ -66,10 +70,10 @@
     .selected {
         border-color: #7d5a5a;
         background-color: #fff4f4;
-        background-image: linear-gradient(90deg, silver 50%, transparent 50%), linear-gradient(90deg, silver 50%, transparent 50%), linear-gradient(0deg, silver 50%, transparent 50%), linear-gradient(0deg, silver 50%, transparent 50%);
+        background-image: linear-gradient(90deg, #5db7ee 50%, transparent 50%), linear-gradient(90deg, #5db7ee 50%, transparent 50%), linear-gradient(0deg, #5db7ee 50%, transparent 50%), linear-gradient(0deg, #5db7ee 50%, transparent 50%);
         background-repeat: repeat-x, repeat-x, repeat-y, repeat-y;
         background-size: 15px 2px, 15px 2px, 2px 15px, 2px 15px;
-        background-position: left top, right bottom, left bottom, right   top;
+        background-position: left top, right bottom, left bottom, right top;
         animation: border-dance 1s infinite linear;
     }
     @keyframes border-dance {
@@ -77,8 +81,35 @@
             background-position: left top, right bottom, left bottom, right   top;
         }
         100% {
-            background-position: left 15px top, right 15px bottom , left bottom 15px , right   top 15px;
+            background-position: left 15px top, right 15px bottom, left bottom 15px, right top 15px;
         }
+    }
+
+    .top-selected {
+        background-image:
+                linear-gradient(90deg, #fff4f4 50%, transparent 50%),
+                linear-gradient(90deg, #5db7ee 50%, transparent 50%),
+                linear-gradient(0deg, #5db7ee 50%, transparent 50%),
+                linear-gradient(0deg, #5db7ee 50%, transparent 50%)
+        !important;
+    }
+
+    .bottom-selected {
+        background-image:
+                linear-gradient(90deg, #5db7ee 50%, transparent 50%),
+                linear-gradient(90deg, #fff4f4 50%, transparent 50%),
+                linear-gradient(0deg, #5db7ee 50%, transparent 50%),
+                linear-gradient(0deg, #5db7ee 50%, transparent 50%)
+        !important;
+    }
+
+    .top-and-bottom-selected {
+        background-image:
+                linear-gradient(90deg, #fff4f4 50%, transparent 50%),
+                linear-gradient(90deg, #fff4f4 50%, transparent 50%),
+                linear-gradient(0deg, #5db7ee 50%, transparent 50%),
+                linear-gradient(0deg, #5db7ee 50%, transparent 50%)
+        !important;
     }
 
 </style>
@@ -97,7 +128,20 @@
       {#each paginatedSource as row, i}
         <tr
           class:selected={selectedTestIDs.includes(row[fields.indexOf("id")])}
-          on:click={() => toggleRow(row)}
+          class:top-selected={
+            selectedTestIDs.includes(row[fields.indexOf("id")]) &&
+            selectedTestIdxes.includes(i - 1)
+          }
+          class:bottom-selected={
+            selectedTestIDs.includes(row[fields.indexOf("id")]) &&
+            selectedTestIdxes.includes(i + 1)
+          }
+          class:top-and-bottom-selected={
+            selectedTestIDs.includes(row[fields.indexOf("id")]) &&
+            selectedTestIdxes.includes(i - 1) &&
+            selectedTestIdxes.includes(i + 1)
+          }
+          on:click={() => toggleRow(row, i)}
         >
           {#each fields as field, j}
             {#if field === "summary"}
